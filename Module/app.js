f09c2847e881ed69975d2b77cffcba65ac078b8b
MercedesDavila <mmdavila@outlook.com>
2019-10-13 17:59:49 -0500

    Curso Udemy NodeJS

---
+++ Module/app.js
@@ -0,0 +1,1 @@
+var sumar = require('./suma');
---
+++ Module/suma.js
@@ -0,0 +1,9 @@
+let sumarDosMasDos = function(){
+    return 2+2;
+}
+
+exports.sumarDosMasDos = sumarDosMasDos;
+
+exports.suma = function(numero1, numero2){
+    return numero1 + numero2;
+}
