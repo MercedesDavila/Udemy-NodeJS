bfcb9ed5adb73b6f0a91b07a0444d547c5653dbf
MercedesDavila <mmdavila@outlook.com>
2019-10-13 19:21:13 -0500

    Módulo Event Built-in NodeJS

---
+++ Module/event.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('event', function(){
+    console.log('Un evento ha ocurrido...');
+})
+
+emitter.emit('event');
