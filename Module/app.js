fd87ae42c10056bd64cfd8f66c406085c2dadff9
MercedesDavila <mmdavila@outlook.com>
2019-10-13 19:14:44 -0500

    Module OS built-in de NodeJS

--- Module/app.js
+++ Module/app.js
@@ -1,1 +1,9 @@
-var sumar = require('./suma');
+const sumar = require('./suma');
+const multiplicar = require('./multiplicar');
+
+console.log(multiplicar.multiplica(5, 6));
+
+
+console.log(sumar.sumarDosMasDos());
+console.log(sumar.suma(6, 9));
+
---
+++ Module/os.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log('Version SO: ', os.release());
+
+console.log('Memoria Libre: ', os.freemem());
+
+console.log("Memoria Total: ", os.totalmem());
+
+
