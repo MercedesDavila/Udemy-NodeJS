5518d64b36162e7436304264a328cedd1e50d498
MercedesDavila <mmdavila@outlook.com>
2020-10-15 18:45:01 -0500

    Modulo event build y HTTP con nodejs

--- Module/event.js
+++ Module/event.js
@@ -7,3 +7,19 @@ emitter.on('event', function(){
 })
 
 emitter.emit('event');
+
+// Evento con argumentos
+
+emitter.on('eventWithArgument', function(arg){
+    console.log('Un evento con los siguientes argumentos ha ocurrido: '  + arg.id + ' ' + arg.numero);
+})
+
+emitter.emit('eventWithArgument', {id:1, numero: 24})
+
+// Evento con funciones flecha
+
+emitter.on('eventArrow', (arg) => {
+    console.log('Un evento flecha con los siguientes argumentos ha ocurrido: '  + arg.id + ' ' + arg.numero);
+})
+
+emitter.emit('eventArrow', {id:1, numero: 24})
